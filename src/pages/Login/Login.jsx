cd90b02874de81850cc589966c86a7bcd3656a34
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-05-04 01:36:41 +0600

    login page added

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,67 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="bg-gray-100 flex flex-col h-screen justify-center items-center">
+      <div className="bg-white rounded-lg p-8 shadow-md">
+        <h2 className="text-3xl font-bold mb-4">Login Your account</h2>
+        <form className="space-y-4">
+          <div>
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter your email address"
+              required
+            />
+          </div>
+          <div>
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="********"
+              required
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Login
+            </button>
+            <Link
+              className="inline-block font-bold text-sm text-blue-500 hover:text-blue-800"
+            >
+              Forgot Password?
+            </Link>
+          </div>
+          <div className="text-center">
+            New to this site?{" "}
+            <Link to="/register" className="text-blue-500">
+              Register here
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
