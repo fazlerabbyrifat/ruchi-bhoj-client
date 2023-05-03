ca10c77c9d57c98a71a19c64b0beceedde950f30
fazlerabbyrifat <rifatfazlerabby@gmail.com>
2023-05-04 01:11:55 +0600

    register page added

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Register = () => {
+  return (
+    <div className="bg-gray-100 flex flex-col h-screen justify-center items-center">
+      <div className="bg-white rounded-lg p-8 shadow-md">
+        <h2 className="text-3xl font-bold mb-4">Create an account</h2>
+        <form className="space-y-4">
+          <div>
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              className="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              name="name"
+              type="text"
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+          <div>
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              name="email"
+              type="email"
+              placeholder="Enter your email address"
+              required
+            />
+          </div>
+          <div>
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="password"
+            >
+              Password
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="password"
+              name="password"
+              type="password"
+              placeholder="********"
+              required
+            />
+          </div>
+          <div>
+            <label
+              className="block text-gray-700 font-bold mb-2"
+              htmlFor="photoUrl"
+            >
+              Photo URL
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="photoUrl"
+              name="image"
+              type="url"
+              placeholder="https://example.com/profile.jpg"
+              required
+            />
+          </div>
+          <div>
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="submit"
+            >
+              Register
+            </button>
+          </div>
+          <div className="text-center">
+            Already have an account?{' '}
+            <Link to="/login" className="text-blue-500">
+              Log in here
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../layouts/Main";
 import Home from "../pages/Home/Home/Home";
 import ChefDetails from "../pages/ChefDetails/ChefDetails";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Register/Register";
 
 const router = createBrowserRouter([
     {
@@ -17,7 +19,15 @@ const router = createBrowserRouter([
                 path: '/chefsinfo/:id',
                 element: <ChefDetails></ChefDetails>,
                 loader: ({params}) => fetch(`http://localhost:5000/chefsinfo/${params.id}`)
-                }
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
                 
         ]
 }
